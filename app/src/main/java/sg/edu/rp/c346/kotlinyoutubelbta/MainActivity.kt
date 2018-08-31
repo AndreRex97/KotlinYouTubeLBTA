41d07be4504eb81eca22dd3ad1c0d9266c5b957e
15017103 <15017103@myrp.edu.sg>
2018-08-31 18:12:15 +0700

    test

--- app/src/main/java/sg/edu/rp/c346/kotlinyoutubelbta/MainActivity.kt
+++ app/src/main/java/sg/edu/rp/c346/kotlinyoutubelbta/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.google.gson.GsonBuilder
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 //        recyclerView_main.setBackgroundColor(Color.BLUE)
 
         recyclerView_main.layoutManager = LinearLayoutManager(this)
+        recyclerView_main.addItemDecoration(DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL))
 
         fetchJson()
     }
@@ -55,20 +58,4 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-//    "id": 1,
-//    "name": "Instagram Firebase - Learn How to Create Users, Follow, and Send Push Notifications",
-//    "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
-//    "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93",
-//    "numberOfViews": 20000,
-//    "channel": {
-//        "name": "Lets Build That App",
-//        "profileImageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/dda5bc77-327f-4944-8f51-ba4f3651ffdf",
-//        "numberOfSubscribers": 100000
-
-    class HomeFeed(val videos: List<Video>)
-
-    class Video (val id: Int, val name: String, val link: String, val imageUrl: String,
-                 val numberOfViews: Int, val channel: Channel)
-
-    class Channel(val name: String, val profileImageUrl: String, val numberOfSubscribers: String)
 }
--- app/src/main/java/sg/edu/rp/c346/kotlinyoutubelbta/MainAdapter.kt
+++ app/src/main/java/sg/edu/rp/c346/kotlinyoutubelbta/MainAdapter.kt
@@ -4,13 +4,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
 /**
  * Created by 15017103 on 1/3/2018.
  */
 
-class MainAdapter(val homeFeed: MainActivity.HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
 
     val videoTitles = listOf("First title", "Second", "3rd", "MOOOOORE TITLE")
 
@@ -31,7 +32,14 @@ class MainAdapter(val homeFeed: MainActivity.HomeFeed): RecyclerView.Adapter<Cus
         val video = homeFeed.videos.get(position)
         holder?.view?.textView_video_title?.text = video.name
 
-        holder?.view?.textView_channel_name?.text = video.channel.name
+        holder?.view?.textView_channel_name?.text = video.channel.name + "  •  " + "20k views \n" +
+                "4 days ago"
+
+        val thumbnailImageView = holder?.view?.imageView_video_thumbnail
+        Picasso.get().load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView = holder?.view?.imageView_channel_profile
+        Picasso.get().load(video.channel.profileImageUrl).into(channelProfileImageView)
     }
 }
 
